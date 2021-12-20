5033f8145e0b642908768e4e1da81df22f8e41b0
Deepu Raj <deepura1@in.ibm.com>
2021-12-20 11:47:10 +0530

    Fixing topology interface error

--- interfaces/Topology.py
+++ interfaces/Topology.py
@@ -2,13 +2,10 @@ from utils.RestUtil import instance as RESTUtil
 
 class Topology:
     def __init__(self,auth):
-        self.rest_url = "/onos/v1/topology"
-        self.auth = auth
+        self.rest_path = "/onos/v1/topology"
 
     def getTopology(self):
-        response = requests.get(self.rest_url,auth=self.auth)
-        response = response.json()
-
+        response = RESTUtil.invoke_rest_api(self.rest_url)
         return response
 
 
